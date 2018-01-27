8915f3305e34425e3886bc0c138bf191f0e42aee
przemyslaw-kamil <przemyslaw.kamil.wojciechowski@gmail.com>
2018-01-27 20:46:56 +0100

    Initial commit, empty project

---
+++ src/test/java/com/przemyslaw_kamil/todolist/Spring5TodolistApplicationTests.java
@@ -0,0 +1,16 @@
+package com.przemyslaw_kamil.todolist;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5TodolistApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
